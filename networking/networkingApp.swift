327424b18bac868f68e7cf8b9f2280c8978ac320
Wildan <wildanalifrio@gmail.com>
2024-05-13 21:15:55 +0700

    Initial Commit

---
+++ networking/networkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  networkingApp.swift
+//  networking
+//
+//  Created by Wildan on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct networkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
